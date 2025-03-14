2bfa8c4866318ecad09254265e01965d92d6b4c4
German <s243660@dtu.dk>
2025-03-14 15:15:47 +0100

    ex 2 missing comments

--- assignment2/assignment2.R
+++ assignment2/assignment2.R
@@ -1,7 +1,7 @@
 #2. Simulating seasonal processes 
 library(astsa)
 
-n <- 100
+n <- 300
 
 plotit <- function(x){
   layout(rbind(1,2:3))
@@ -11,37 +11,38 @@ plotit <- function(x){
   pacf(x, lag.max=50, lwd=2)
 }
 #2.1 A (1,0,0) ×(0,0,0)12 model with the parameter φ1 = 0.6.
-set.seed(1)
-y <- arima.sim(n = n, list(ar = c(0.6)))
+set.seed(11)
+y <- arima.sim(n = n, model = list(ar = c(-0.6)))
 plotit(y)
 
+
 #2.2. A (0,0,0) ×(1,0,0)12 model with the parameter Φ1 = −0.9.
-set.seed(2)
-y <- sarima.sim(n = n, sar=c(-0.9), S=12)
+set.seed(12)
+y <- arima.sim(n = 100, list(ar = c(rep(0,11), 0.9)))
 plotit(y)
 
 #2.3
 #A (1,0,0) ×(0,0,1)12 model with the parameters φ1 = 0.9 and Θ1 = −0.7
 set.seed(3)
-y <- sarima.sim(n = n, ar= 0.9, sma = -0.7, S=12)
+y <- arima.sim(n = n, model = list(ar = c(-0.9), ma = c(rep(0,11), -0.7)))
 plotit(y)
 
 #2.4
 #A (1,0,0) ×(1,0,0)12 model with the parameters φ1 = −0.6 and Φ1 = −0.8
-set.seed(4)
-y <- sarima.sim(n = n, ar= -0.6, sar=c(-0.8), S=12)
+set.seed(10)
+y <- arima.sim(list(ar=c(0.6,rep(0,10),0.8,-0.6*0.8)),n)
 plotit(y)
 
 #2.5
 #A (0,0,1) ×(0,0,1)12 model with the parameters θ1 = 0.4 and Θ1 = −0.8
-set.seed(5)
-
-y <- sarima.sim(n = n, ma= 0.4, sma = -0.8, S=12)
+set.seed(15)
+y <- arima.sim(n = n, model = list(ma = c(0.4, rep(0,10), -0.8)))
 plotit(y)
 
 
 #2.6
 #A (0,0,1) ×(1,0,0)12 model with the parameters θ1 = −0.4 and Φ1 = 0.7
 set.seed(6)
-y <- sarima.sim(n = n, ma= -0.4, sar=c(0.7), S=12)
+y <- arima.sim(n = n, model = list(ma = c(-0.4), ar = c(rep(0,11), -0.7)))
 plotit(y)
+
